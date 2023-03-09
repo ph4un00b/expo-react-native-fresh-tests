a2d45b210195e97651d105b39d3ee68808743985
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-03-09 16:51:59 -0600

    chore

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("log!")
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>HOLA 🎈 up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
